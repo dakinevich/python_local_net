fce1aa257b792c7f465dddf2f20dd96129bbd4f7
ivan <ivan7855060@gmail.com>
2022-11-11 10:52:39 +0300

    da

--- client.py
+++ client.py
@@ -1,39 +1,7 @@
+#client.py
 import socket
-import select
-import sys
- 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-if len(sys.argv) != 3:
-    print ("Correct usage: script, IP address, port number")
-    exit()
-IP_address = str(sys.argv[1])
-Port = int(sys.argv[2])
-server.connect((IP_address, Port))
- 
-while True:
- 
-    # maintains a list of possible input streams
-    sockets_list = [sys.stdin, server]
- 
-    """ There are two possible input situations. Either the
-    user wants to give manual input to send to other people,
-    or the server is sending a message to be printed on the
-    screen. Select returns from sockets_list, the stream that
-    is reader for input. So for example, if the server wants
-    to send a message, then the if condition will hold true
-    below.If the user wants to send a message, the else
-    condition will evaluate as true"""
-    read_sockets,write_socket, error_socket = select.select(sockets_list,[],[])
- 
-    for socks in read_sockets:
-        if socks == server:
-            message = socks.recv(2048)
-            print (message)
-        else:
-            message = sys.stdin.readline()
-            my_str_as_bytes = str.encode(message)
-            server.send(my_str_as_bytes)
-            sys.stdout.write("<You>")
-            sys.stdout.write(message)
-            sys.stdout.flush()
-server.close()
+my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host = "localhost" # "127.0.1.1"
+port = 8000
+my_socket.connect((host, port))
+my_socket.send("hello".encode())
